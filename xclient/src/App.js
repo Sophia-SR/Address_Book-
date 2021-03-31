998ad45e76c840ed14f0f8144a5cca3808a26479
Sophia Rafat <sophiar.rafat@gmail.com>
2021-03-31 16:20:16 -0500

    commit

---
+++ xclient/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AddContact from './components/addContact';
+import ListContacts from './components/listContacts';
+import SearchContacts from './components/searchContacts'
+
+function App() {
+  return (
+    <>
+    <AddContact/>
+    <ListContacts/>
+    <SearchContacts/>
+    </>
+  );
+}
+
+export default App;
